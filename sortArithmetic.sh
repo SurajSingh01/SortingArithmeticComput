e8d4b399526aaaba719a5a5e51ab61e1a19b6edd
SurajSingh01 <singhsurajd03@gmail.com>
2020-06-04 10:01:03 +0530

    Storing result in Dictionary

--- sortArithmetic.sh
+++ sortArithmetic.sh
@@ -5,8 +5,10 @@ read -p "Enter value of a : " a
 read -p "Enter value of b : " b
 read -p "Enter Value of c : " c
 
-result1=$(( $a + $b * $c ))
-result2=$(( $a * $b + $c ))
-result3=$(( $c + $a / $b ))
-result4=$(( $a % $b + $c ))
+declare -A arrayNum
+arrayNum[result1]=$(( $a + $b * $c ))
+arrayNum[result2]=$(( $a * $b + $c ))
+arrayNum[result3]=$(( $c + $a / $b ))
+arrayNum[result4]=$(( $a % $b + $c ))
 
+echo ${!arrayNum[@]}
